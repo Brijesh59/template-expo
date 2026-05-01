5e3cad1d10079eddee2e8eb618285088e83dcb67
Brijesh <kumarbrijesh9021@gmail.com>
2026-05-01 08:35:40 +0530

    cleanup

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,9 @@
-import { StyleSheet, Text, View } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
-
+import { Text, View } from 'react-native';
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text className='text-3xl'>Tab One</Text>
-      <View style={styles.separator}  />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-xl">Tab 1</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
